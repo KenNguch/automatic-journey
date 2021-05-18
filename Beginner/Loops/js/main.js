4a05bbbc00a57e8355be698a1dc635c1a14c6aa3
KenNguch <kennguch3@gmail.com>
2021-05-18 09:55:43 +0300

    loops done

---
+++ Beginner/Loops/js/main.js
@@ -0,0 +1,10 @@
+var count = prompt("Enter A Number to use in the loop", 0);
+
+document.write("Loop Started");
+
+while (count < 10) {
+    document.write("Current Count is : " + count + "<br />");
+    count++;
+
+}
+document.write("The End")
